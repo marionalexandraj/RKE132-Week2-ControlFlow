31f39286dfb972acffa5d56ab10afc19ed07d75c
= <=>
2024-05-31 22:55:40 +0300

    initial commit

---
+++ UserGenderValidator/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please, enter your last name:");
+string userLastName = Console.ReadLine();
+
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}!");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {userLastName}!");
+}
